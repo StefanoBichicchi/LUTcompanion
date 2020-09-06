08af850ed920101b5c093a93a85c4516c3249526
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-09-06 11:56:04 +0200

    add load settings

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,42 @@
   </div>
 </template>
 <script>
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+
+  methods: {
+    loadUserSettings() {
+      const dirName = "lut_companion";
+      const fileName = "settings.json";
+
+      const dirPath =
+        this.$q.electron.remote.app.getPath("appData") + "/" + dirName;
+      const filePath = dirPath + "/" + fileName;
+
+      if (!existsSync(dirPath)) mkdirSync(dirPath);
+
+      if (!existsSync(filePath)) {
+        const data = require("./data/sample_config.json");
+        writeFileSync(filePath, JSON.stringify(data, null, 2));
+      }
+
+      const userSettings = JSON.parse(readFileSync(filePath));
+
+      this.$q.sessionStorage.set("userSettings", userSettings);
+    },
+
+    setUserSettings() {
+      const userSettings = this.$q.sessionStorage.getItem("userSettings");
+      this.$q.dark.set(userSettings.theme.enableDark);
+      this.$i18n.locale = userSettings.language;
+    },
+  },
+
+  mounted() {
+    this.loadUserSettings();
+    this.setUserSettings();
+  },
+};
 </script>
